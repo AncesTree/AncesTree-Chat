60e04afabacef1b2c6866fc860d422628f8c030e
PaulArnaud <paul.arnaud@etu.umontpellier.fr>
2019-11-19 15:03:06 +0100

    Remove db.remove

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ const connect = mongoose.connect(url, {
   useNewUrlParser: true
 });
 const db = mongoose.connection
-db.dropDatabase();
+//db.dropDatabase();
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('connected to database'))
 
